d64d3c339b9199ff9b6dacb001720586d5388d12
Paulo Vilela <paulov4@hotmail.com>
2025-06-23 19:11:21 -0300

    Subtotal & Total methods

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -64,16 +64,18 @@ public class TestConfig implements CommandLineRunner {
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u3);
         Order o4 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.DELIVERED, u3);
+        Order o5 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"),OrderStatus.PAID, u3);
 
         userRepository.saveAll(Arrays.asList(u1, u2, u3));
-        orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3, o4, o5));
 
         OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
         OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
         OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+        OrderItem oi5 = new OrderItem(o3, p1, 1, p1.getPrice());
 
-        orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+        orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4, oi5));
 
         Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o3);
         o1.setPayment(pay1);
